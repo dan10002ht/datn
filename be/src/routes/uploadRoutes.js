2f91ea1a899343c1b486a005aeecf29afdfc76a7
dantt25 <dantt@avadagroup.com>
2024-06-20 13:33:34 +0700

    add image

--- be/src/routes/uploadRoutes.js
+++ be/src/routes/uploadRoutes.js
@@ -6,7 +6,12 @@ const router = new Router();
 router.post('/', upload.single('image'), async (req, res) => {
   const {file} = req;
   const response = await handleUpload(file);
-  return res.status(200).json({success: true, data: response});
+  return res.status(200).json({
+    success: true,
+    data: {
+      url: response.url,
+    },
+  });
 });
 
 export default router;
