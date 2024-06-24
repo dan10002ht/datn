2730068adca05575c2830e36c6329f8c36873fb5
dantt2 <dantt1002@gmail.com>
2024-06-24 22:39:40 +0700

    fix minor

--- fe/src/pages/Home.jsx
+++ fe/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import RequestBox from "../components/molecules/RequestBox";
 import HistoryBox from "../components/molecules/HistoryBox";
 import TimeChart from "../components/molecules/TimeChart";
